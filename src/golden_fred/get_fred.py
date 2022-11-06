5b4a124ccb48fddc0eeebb57b0c17fb5c63f36d3
gombrowicz94 <alex.aronovich@princeton.edu>
2022-11-06 16:12:17 -0500

    Added checker for vintage fed into GetFred

--- src/golden_fred/get_fred.py
+++ src/golden_fred/get_fred.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 from urllib import request
-from warnings import warn
+from datetime import date
+import warnings
 import os
 
 
@@ -12,6 +13,7 @@ class GetFred:
 
     def __init__(self, vintage="current"):
         self.vintage = vintage  # if not default "current" MUST be YYYY-MM
+        self._check_vintage()
         self.url_format = "https://files.stlouisfed.org/files/htdocs/fred-md/"
 
     def get_fredmd(self):
@@ -29,3 +31,31 @@ class GetFred:
         url = f"{self.url_format}/{freq}/{self.vintage}.csv"
         response = request.urlopen(url)
         csv = response.read()
+
+    def _check_vintage(self):
+        """
+        A verification function to ensure a proper vintage is being fed into the class.
+        It will throw an exception if any issues
+        :return: a warning if vintage != default or Exception if vintage isn't proper.
+        """
+        if self.vintage == "current":
+            pass
+        else:
+            warnings.warn(
+                f"""It is advised to use the default vintage: current.
+                          If requesting a historical vintage, use format YYYY-MM.
+                          Oldest vintage is 2015-01."""
+            )
+            if "-" not in self.vintage:
+                raise Exception(
+                    f"Incorrect vintage format: {self.vintage}. Correct format: YYYY-MM"
+                )
+            year, mon = self.vintage.split("-")
+            if (year < 2015) or (year > date.today().year):
+                raise Exception(f"Invalid year: {year}. Format YYYY-MM.")
+            if (mon > 12) or (mon < 1):
+                raise Exception(f"Invalid month: {mon}. Format YYYY-MM")
+            if (year == date.today().year) and (mon > date.today().month):
+                raise Exception(
+                    f"Vintage {self.vintage} too far into the future. Request vintage=current for latest data"
+                )
