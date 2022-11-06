662c9db359d899374f40dc343b9cf17165e8d4b8
gombrowicz94 <alex.aronovich@princeton.edu>
2022-11-06 18:04:01 -0500

    add date filtering and typing

--- src/golden_fred/get_fred.py
+++ src/golden_fred/get_fred.py
@@ -1,18 +1,29 @@
 import pandas as pd
 import numpy as np
 from urllib import request
-from datetime import date
+import datetime
 import warnings
-import os
+from typing import Union
 
 
 class GetFred:
-    """
-    Pull FRED-MD or FRED-QD data
-    """
-
-    def __init__(self, transform=True, vintage="current"):
+    def __init__(
+        self,
+        transform: bool = True,
+        start_date: Union[datetime, None] = None,
+        end_date: Union[datetime, None] = None,
+        vintage: str = "current",
+    ):
+        """
+        Pull FRED-MD or FRED-QD data. Returns a Pandas DataFrame of golden copy FRED data.
+        :param transform: transform to stationarity
+        :param start_date: start date for data
+        :param end_date: end date for data
+        :param vintage: which version of the file to look at; 'current' uses the latest one
+        """
         self.transform = transform
+        self.start_date = start_date
+        self.end_date = end_date
         self.vintage = vintage  # if not default "current" MUST be YYYY-MM
         self._check_vintage()
         self.url_format = "https://files.stlouisfed.org/files/htdocs/fred-md/"
@@ -26,14 +37,14 @@ class GetFred:
             7: lambda l: (l / l.shift(1) - 1).diff(),
         }
 
-    def get_fredmd(self):
+    def get_fredmd(self, freq: str = "monthly") -> pd.DataFrame:
         raw_df = self._get_file(freq="monthly")
         df, transf_codes = self._clean_df(raw_df)
         if self.transform:
             df = self._stationarize(df, transf_codes)
         return df
 
-    def _stationarize(self, df, transf_codes):
+    def _stationarize(self, df: pd.DataFrame, transf_codes: pd.Series) -> pd.DataFrame:
         df_trans = []
         for s in range(df.shape[1]):  # perform transformations
             s_trans = self.stationarity_functions[transf_codes[s]](df.iloc[:, s])
@@ -41,7 +52,7 @@ class GetFred:
         df_trans = pd.DataFrame(df_trans).T.dropna(how="all")
         return df_trans
 
-    def _clean_df(self, raw_df):
+    def _clean_df(self, raw_df: pd.DataFrame) -> pd.DataFrame:
         """
         Return a cleaned dataframe with transformation codes taken out and a nice
         indexed date.
@@ -56,7 +67,7 @@ class GetFred:
         df = df.set_index("date")
         return df, transf_codes
 
-    def _get_file(self, freq="monthly"):
+    def _get_file(self, freq: str = "monthly") -> pd.DataFrame:
         """
         Pull the source file from the FRED-MD site into Pandas DataFrame.
         :param freq: 'monthly' or 'quarterly'
@@ -64,6 +75,18 @@ class GetFred:
         """
         url = f"{self.url_format}/{freq}/{self.vintage}.csv"
         df = pd.read_csv(url)
+        return df
+
+    def _filter_dates(
+        self,
+        df: pd.DataFrame,
+        start_date: Union[datetime, None],
+        end_date: Union[datetime, None],
+    ) -> pd.DataFrame:
+        if start_date:
+            df = df.loc[self.start_date :]
+        if end_date:
+            df = df.loc[self.end_date :]
         return df
 
     def _check_vintage(self):
@@ -85,11 +108,13 @@ class GetFred:
                     f"Incorrect vintage format: {self.vintage}. Correct format: YYYY-MM"
                 )
             year, mon = self.vintage.split("-")
-            if (year < 2015) or (year > date.today().year):
+            if (year < 2015) or (year > datetime.date.today().year):
                 raise Exception(f"Invalid year: {year}. Format YYYY-MM.")
             if (mon > 12) or (mon < 1):
                 raise Exception(f"Invalid month: {mon}. Format YYYY-MM")
-            if (year == date.today().year) and (mon > date.today().month):
+            if (year == datetime.date.today().year) and (
+                mon > datetime.date.today().month
+            ):
                 raise Exception(
                     f"Vintage {self.vintage} too far into the future. Request vintage=current for latest data"
                 )
