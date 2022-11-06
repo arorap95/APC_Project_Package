9c96b979165cb9788195cb046206787d1f18ca84
gombrowicz94 <alex.aronovich@princeton.edu>
2022-11-06 15:56:26 -0500

    reformatted using black

--- src/golden_fred/get_fred.py
+++ src/golden_fred/get_fred.py
@@ -9,23 +9,23 @@ class GetFred:
     """
     Pull FRED-MD or FRED-QD data
     """
-    def __init__(self,vintage="current"):
-        self.vintage = vintage # if not default "current" MUST be YYYY-MM
-        self.url_format = 'https://files.stlouisfed.org/files/htdocs/fred-md/'
+
+    def __init__(self, vintage="current"):
+        self.vintage = vintage  # if not default "current" MUST be YYYY-MM
+        self.url_format = "https://files.stlouisfed.org/files/htdocs/fred-md/"
+
     def get_fredmd(self):
-        df = self._get_file(freq='monthly')
+        df = self._get_file(freq="monthly")
 
-
-    def _get_file(self,freq='monthly'):
+    def _get_file(self, freq="monthly"):
         """
         Pull the source file from the FRED-MD site into Pandas DataFrame.
         :param freq: 'monthly' or 'quarterly'
         :return: Pandas DataFrame with raw results
         """
-        if freq not in ['monthly','quarterly']:
-            raise Exception(f'Invalid frequency: {freq}. Must be monthly or quarterly.')
+        if freq not in ["monthly", "quarterly"]:
+            raise Exception(f"Invalid frequency: {freq}. Must be monthly or quarterly.")
 
         url = f"{self.url_format}/{freq}/{self.vintage}.csv"
         response = request.urlopen(url)
         csv = response.read()
-
