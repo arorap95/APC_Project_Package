8e6bfe1ef446863921f7e28a254a575965d15654
gombrowicz94 <alex.aronovich@princeton.edu>
2022-11-06 15:19:02 -0500

    fixing black format issue

--- src/golden_fred/get_fred.py
+++ src/golden_fred/get_fred.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 
-class get_fred(vintage='current'):
+
+class get_fred(vintage="current"):
     def __init__(self):
         self.vintage = vintage
-
