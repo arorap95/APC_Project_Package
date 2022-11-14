be62ae3db690e5803c39007e81e0808c3fc2e2aa
gombrowicz94 <alex.aronovich@princeton.edu>
2022-11-14 13:17:09 -0500

    testing fake test

---
+++ tests/test_data.py
@@ -0,0 +1,11 @@
+from golden_fred import fred_factors
+import pytest
+import random
+import numpy as np
+import datetime
+import pandas as pd
+from numpy.linalg import eigh
+
+
+def test():
+    assert 4 == 2 * 2
