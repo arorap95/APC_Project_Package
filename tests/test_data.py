9655bfdfc9d3a2075000b717d508f5e63d0dcf81
gombrowicz94 <alex.aronovich@princeton.edu>
2022-11-16 00:16:57 -0500

    fixing fixture issues in testing...

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,9 +6,7 @@ import pandas as pd
 import numpy as np
 from pandas.testing import assert_frame_equal
 
-
 sample_data = pd.Series(np.random.randint(0, 100, 100))
-
 
 @pytest.fixture
 def c():
@@ -33,7 +31,7 @@ def test_init(c, start_date):
 
 
 @pytest.mark.parametrize("transf_code", [1, 2, 3, 4, 5, 6, 7])
-def test_stationarity(sample_data, transf_code):
+def test_stationarity(c,sample_data, transf_code):
     myobject = c()
     if transf_code == 1:
         assert sample_data == myobject.stationarity_functions[1](sample_data)
@@ -58,7 +56,7 @@ def test_stationarity(sample_data, transf_code):
 
 
 @pytest.mark.parametrize("vintage", ["2015-06", "2017-02", "2019-04"])
-def test_vintage_start(vintage):
+def test_vintage_start(c,vintage):
     """
     Ensure the last date is not after the vintage date. Ensure the first date is not before
     the start date of the data.
@@ -71,7 +69,7 @@ def test_vintage_start(vintage):
 
 
 @pytest.mark.parametrize("vintage", ["random", "2017-13", "2300-04"])
-def test_vintage_format(vintage):
+def test_vintage_format(c,vintage):
     """
     Ensure that putting in invalid `vintage` format throws an error.
     """
@@ -82,7 +80,7 @@ def test_vintage_format(vintage):
 @pytest.mark.parametrize(
     "fred_qd_group", [[1, 2], [4, 7], [5, 8, 9], [10, 11, 12], None]
 )
-def test_duplicates(fred_qd_group):
+def test_duplicates(c,fred_qd_group):
     """
     The purpose of this function is to test that combine_fred leads to NO exact
     duplicate columns. This is very important, as it is tricky to ensure that the
@@ -95,7 +93,7 @@ def test_duplicates(fred_qd_group):
 
 
 # not parametrizing due to different object dictionary names
-def test_appendix_cols(freq):
+def test_appendix_cols(c,freq):
     """
     The purpose of this function is to ensure that the groups saved in the class object
     correspond to the NUMBER of groups in the appendix for both QD and MD. If it breaks,
@@ -112,7 +110,7 @@ def test_appendix_cols(freq):
     assert unique_groups_qd == len(myobject.group_lookup["FRED-QD"])
 
 
-def test_interp_freq():
+def test_interp_freq(c):
     """
     Ensure that the interpolation frequency stays at the beginning of the month
     in the index.
@@ -132,16 +130,16 @@ def test_interp_freq():
         pd.Timestamp("2022-01-01"),
     ],
 )
-def test_interp_freq(date):
+def test_interp_freq(c,date):
     """
     Test that the interpolation happens BEFORE the stationarity.
     This would be a big mistake if not done: it could lead to countless econometric
     issues, and it is easily done by flipping the order of the ``get()`` class.
     """
-    myobject_stationary = get_fred.GetFred(transform=True)
-    myobject_levels = get_fred.GetFred(transform=False)
-    qd_stationary = myobject_stationary.get_fred_qd(interpolate_to_monthly=True)
-    qd_levels = myobject_levels.get_fred_qd(interpolate_to_monthly=False)
+    myobject_stationary = c(transform=True)
+    myobject_levels = c(transform=False)
+    qd_stationary = c(interpolate_to_monthly=True)
+    qd_levels = c(interpolate_to_monthly=False)
     qd_levels_interp = qd_levels.resample("MS").interpolate()
     transf_codes = myobject_levels._clean_qd(
         myobject_levels._get_file(freq="quarterly")
@@ -155,7 +153,7 @@ def test_interp_freq(date):
 
 
 @pytest.mark.parametrize("group_no", [[1], [2], [3], [1, 2], [8], [4, 5, 6, 7]])
-def test_md_group_varcount(group_no):
+def test_md_group_varcount(c,group_no):
     """
     Determine the counts of variables from pulling the full dataset for a given
     group number is equal to the number of variables in the appendix for that group.
@@ -173,7 +171,7 @@ def test_md_group_varcount(group_no):
 @pytest.mark.parametrize(
     "group_no", [[1], [2], [3], [1, 2], [8], [4, 5, 6, 7], [11, 12]]
 )
-def test_qd_group_varcount(group_no):
+def test_qd_group_varcount(c,group_no):
     """
     Determine the counts of variables from pulling the full dataset for a given
     group number is equal to the number of variables in the appendix for that group.
@@ -188,7 +186,7 @@ def test_qd_group_varcount(group_no):
     assert col_counts == len(appendix_df_filtered)
 
 
-def test_descriptions_md():
+def test_descriptions_md(c):
     """
     If you replace variable names with descriptions, then there should
     no longer be variable names and only descriptions.
@@ -204,7 +202,7 @@ def test_descriptions_md():
     )  # there are 4 that should match: the S&P ones
 
 
-def test_descriptions_qd():
+def test_descriptions_qd(c):
     """
     If you replace variable names with descriptions, then there should
     no longer be variable names and only descriptions. This is specific to QD.
